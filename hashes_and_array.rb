d4a10aff48eb9f0fb4b6433b161121bb7b0eb1fd
marizaswart <mariza.swart@gmail.com>
2017-11-06 09:00:26 +0100

    Add hashes and var files

---
+++ hashes_and_array.rb
@@ -0,0 +1,5 @@
+car1 = { wheels: 4, max_speed: 220, color: 'black' }
+car2 = { wheels: 4, max_speed: 300, color: 'silver' }
+
+cars = [car1, car2]
+cars[1][:color]
---
+++ variables.rb
@@ -0,0 +1,6 @@
+variable = 'variable'
+Data types
+'sting' - 'This is a string'
+fixnum - 31
+float - 31,5
+booleans - true or false
